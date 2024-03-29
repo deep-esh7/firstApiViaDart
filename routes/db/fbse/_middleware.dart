9c15f18364d4fea278c3d7aeeb3cb92961710b9c
Deepesh Kumar <dkdeepeshkumar5@gmail.com>
2024-03-29 15:43:34 +0530

    ready to test api2

--- routes/db/fbse/_middleware.dart
+++ routes/db/fbse/_middleware.dart
@@ -1,44 +1,6 @@
-import 'dart:convert';
-
-import 'package:firebase_dart/core.dart';
-import 'package:firebase_dart/implementation/pure_dart.dart';
-import 'package:firebase_dart/storage.dart';
-import 'package:firedart/firedart.dart';
-
 import 'package:dart_frog/dart_frog.dart';
-
-import '../../../lib/config.dart';
-
-
+import 'package:first_api/cors.dart';
 
 Handler middleware(Handler handler) {
-  // TODO: implement middleware
-  return (context) async {
- 
-
-    if (Firestore.initialized == false) {
-      Firestore.initialize("healthcare7-7476f");
-          FirebaseDart.setup();
-      await Firebase.initializeApp(options: FirebaseOptions.fromMap(config));
-    }
- 
-    //  if (!FirebaseAuth.initialized) {
-    //   FirebaseAuth.initialize(
-    //       'AIzaSyDcHpc-LRPCX_M9_kLoCtls0ExhByUe7TE', VolatileStore());
-    //           await FirebaseAuth.instance
-    //     .signInAnonymously();
-    // }
-
-    // if(FirebaseAuth.instance.isSignedIn==false && FirebaseAuth.initialized) {
-    // await FirebaseAuth.instance
-    //     .signInAnonymously();
-
-    // }
-         handler.use(requestLogger());
-
-    final response = await handler(context);
-
-    return response;
-    
-  };
+  return handler.use(requestLogger()).use(corsHeaders());
 }
